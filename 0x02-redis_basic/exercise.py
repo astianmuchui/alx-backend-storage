ef3fa386d75e083eac514e6dba913cab012a55bb
astianmuchui <astianmuchui@gmail.com>
2024-04-23 09:35:51 +0300

    Pycodestyle validation

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Exercise
+"""
+
+import redis
+import uuid
+from typing import Union, Callable, Optional, List
+
+
+class Cache:
+    """
+    Cache class
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in redis
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
