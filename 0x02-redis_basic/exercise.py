c910d2b931d556fd594a336f8d85e04a76b2cf9a
astianmuchui <astianmuchui@gmail.com>
2024-04-23 09:44:34 +0300

    Implemented get method and helpers

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -27,3 +27,25 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get_int(self, key: str) -> int:
+        """
+        Get int from redis
+        """
+        return int(self.get(key))
+
+    def get_str(self, key: str) -> str:
+        """
+        Get string from redis
+        """
+        return self.get(key)
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """
+        Get data from redis
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
